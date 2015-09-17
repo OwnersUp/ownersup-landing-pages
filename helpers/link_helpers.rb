82ca61763caf7010d96600fe687a99244bb6492d
Matt McManus <matt@mcmanus.io>
2015-09-17 16:38:16 -0400

    Colors, content and tidying up

--- helpers/link_helpers.rb
+++ helpers/link_helpers.rb
@@ -1,12 +1,16 @@
 module LinkHelpers
   def nav_link(link_text, page_url, options = {})
-    options[:class] ||= "nav-link"
+    options[:class] ||= ""
+
     if current_page.url.length > 1
       current_url = current_page.url.chop
     else
       current_url = current_page.url
     end
-    options[:class] << " active" if page_url == current_url
+
+    if page_url == current_url || current_url.start_with?(page_url)
+      options[:class] << " active"
+    end
 
     content_tag :li, options do
       link_to(link_text, page_url)
--- source/faq/account/how-do-cancel-my-account.md
+++ source/faq/account/how-do-cancel-my-account.md
@@ -2,4 +2,4 @@
 title: How do I cancel my account?
 category: account
 ---
-There are ways
+We currently do not have support for automatted cancellation of your account. If you no longer wish to continue participating in OwnersUp, please contact [support](mailto:support@ownersup.com) and we'll clear it up as soon as possible.
